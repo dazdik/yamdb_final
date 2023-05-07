5ce0c231f32e8210fc7b677494a87633454a3509
Дарья Коннова <dashustic@yandex.ru>
2023-05-07 16:37:48 +0300

    8

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -10,7 +10,12 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    '51.250.75.35',
+    'localhost',
+    '127.0.0.1',
+    'dazdik.sytes.net',
+]
 
 # Application definition
 
